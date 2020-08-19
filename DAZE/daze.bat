c76e2c4aa6631257495d492e714fd1b87575f1f9
randy orton <rebeccalane27@gmail.com>
2020-08-19 23:41:04 +0000

    Update daze.bat

--- DAZE/daze.bat
+++ DAZE/daze.bat
@@ -1,5 +1,5 @@
 CD /D "%~dp0"
-start daze_windows_386.exe client -s 45.89.197.71:12345 -k dongtaiwang.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
+start daze_windows_386.exe client -s 64.32.6.246:12345 -k dongtaiwang.com -l 127.0.0.1:3080 -dns 8.8.8.8:53
 exit
 
 
