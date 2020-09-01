1a2f2dcb84417b06ba998557d0e1eda6b0a970dc
randy orton <rebeccalane27@gmail.com>
2020-09-01 05:09:38 +0000

    Update goflyway.bat

--- goflyway/goflyway.bat
+++ goflyway/goflyway.bat
@@ -1,5 +1,4 @@
 CD /D "%~dp0"
-goflyway -l="127.0.0.1:8100"  -up="89.163.224.92:8880"  -k="ntdtv.com 123abc"
+goflyway -l="127.0.0.1:8100"  -up="89.163.212.34:12345"  -k="dongtaiwang.com 123abc"
 pause
 exit
-
