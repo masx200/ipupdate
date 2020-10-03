984f9698c0ac8e8f09acf9a4f1914fb02ad00bc4
randy orton <rebeccalane27@gmail.com>
2020-10-03 01:07:11 +0000

    Update brook.bat

--- brook/brook.bat
+++ brook/brook.bat
@@ -1,8 +1,6 @@
 CD /D "%~dp0"
-start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 67.21.81.19:11111 -p ntdtv.com
+start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 67.21.81.19:33333 -p dongtaiwang.com
 exit
-
-
 
 
 
