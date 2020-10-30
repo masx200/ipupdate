1e4537cd56d524299a242460bbd742b129e60a0f
randy orton <rebeccalane27@gmail.com>
2020-10-30 02:55:30 +0000

    Update brook.bat

--- brook/brook.bat
+++ brook/brook.bat
@@ -1,5 +1,5 @@
 CD /D "%~dp0"
-start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 134.122.103.186:12345 -p dongtaiwang.com
+start brook client -l 127.0.0.1:2080 -i 127.0.0.1 -s 206.189.214.101:12345 -p dongtaiwang.com
 exit
 
 
